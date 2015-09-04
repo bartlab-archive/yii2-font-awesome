39421bc87252888881d71b42fab2374c55246d96
B@RT <bartlab@gmail.com>
2015-09-04 13:24:09 +0300

    Create FontAwesomeAsset.php

---
+++ FontAwesomeAsset.php
@@ -0,0 +1,11 @@
+<?php
+namespace maybeworks\fontawesome;
+
+use \yii\web\AssetBundle;
+
+class FontAwesomeAsset extends AssetBundle {
+	public $sourcePath = '@bower/font-awesome';
+	public $css = [
+		'css/font-awesome.min.css',
+	];
+}
