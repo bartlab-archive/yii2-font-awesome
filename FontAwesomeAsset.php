17a198a7db4a20fa012e339cf5f7c19d2944e7b7
B@RT <bartlab@gmail.com>
2018-08-07 20:07:23 +0300

    Update FontAwesomeAsset.php

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -6,6 +6,7 @@ use \yii\web\AssetBundle;
 class FontAwesomeAsset extends AssetBundle {
 	public $sourcePath = '@bower/font-awesome';
 	public $css = [
-		'css/font-awesome.min.css',
+		'web-fonts-with-css/css/fontawesome-all.min.css',
+		'web-fonts-with-css/css/v4-shims.min.css',
 	];
 }
